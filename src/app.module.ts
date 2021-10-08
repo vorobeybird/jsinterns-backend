67b45fb94ec4610277135775c398e544787b070d
Valery.Prankevich <vprankevich@gmail.com>
2021-10-08 17:00:20 +0300

    Initial local dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/pinktada'),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'db',
