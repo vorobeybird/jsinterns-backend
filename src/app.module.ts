cbc66a65af2a60d33bfc73b062379bf8f29719b7
Valery.Prankevich <vprankevich@gmail.com>
2021-10-15 17:36:48 +0300

    Test MySQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,18 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  // imports: [
-  //   MongooseModule.forRoot('mongodb://localhost:27017/pinktada'),
-  //   TypeOrmModule.forRoot({
-  //     type: 'mysql',
-  //     host: 'db',
-  //     port: 3306,
-  //     username: 'test',
-  //     password: 'test',
-  //     database: 'test',
-  //     entities: [],
-  //     synchronize: true,
-  //   }),
-  // ],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '10.0.0.213',
+      port: 3306,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
